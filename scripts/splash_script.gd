21ff88828452df54db6ce8fe1ea755e2bf2ea1bf
KingGedorah <naufal.zhafari@ui.ac.id>
2024-05-27 14:38:04 +0700

    splash screen fix

--- scripts/splash_script.gd
+++ scripts/splash_script.gd
@@ -1,13 +1,12 @@
 extends Node
 class_name SplashScreen
 
-
+@export var current_splash : int = 1
 
 func _input(event : InputEvent): 
-	var count : int = 1
 	if(event.is_action_pressed("action")):
-		count = count + 1
-		if count == 4:
+		var next_number = current_splash+ 1
+		if next_number == 4:
 			get_tree().change_scene_to_file("res://scenes/menu.tscn")
 		else:
-			get_tree().change_scene_to_file("res://scenes/SplashScreen#" + str(count) + ".tscn")
+			get_tree().change_scene_to_file("res://scenes/SplashScreen#" + str(next_number) + ".tscn")
