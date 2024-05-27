b76ec3487c507319cb193d576e937989dd464e5e
KingGedorah <naufal.zhafari@ui.ac.id>
2024-05-27 14:25:59 +0700

    splash screen

--- scripts/splash_script.gd
+++ scripts/splash_script.gd
@@ -2,13 +2,12 @@ extends Node
 class_name SplashScreen
 
 
-func _input(event : InputEvent):
+
+func _input(event : InputEvent): 
+	var count : int = 1
 	if(event.is_action_pressed("action")):
-		var node_name = get_tree().get_root()
-		if ("#") in node_name:
-			var current_number : int = int(node_name.get_slice("#",1))
-			var next_number : int = current_number + 1
-			if next_number == 4:
-				return "res://scenes/menu.tscn"
-			else:
-				return "res://scenes/SplashScreen#" + str(next_number) + ".tscn"
+		count = count + 1
+		if count == 4:
+			get_tree().change_scene_to_file("res://scenes/menu.tscn")
+		else:
+			get_tree().change_scene_to_file("res://scenes/SplashScreen#" + str(count) + ".tscn")
